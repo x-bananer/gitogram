27c070aa33081d977472b7a7b0572fcfa6089de8
Ksenia Shlyonskaya <xeniya.shlyonskaya@gmail.com>
2021-09-09 23:14:40 +0300

    fix gh api

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,10 @@
 module.exports = {
-  "stories": ["../src/components/**/*.stories.js"],
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ],
+  ]
 }
